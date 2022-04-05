bbb9c3fe8c1859a800194e75c8382f6a6d8205b2
ulugbek <zaripovulugbek45@gmail.com>
2022-04-05 23:18:49 +0500

    changed

--- urlshortner/urlapp/models.py
+++ urlshortner/urlapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class LinkInfo(models.Model):
     link = models.CharField(max_length=10000)
-    link_id = models.CharField(max_length=5)
+    link_id = models.CharField(max_length=8)
 
     def __str__(self):
         return self.link_id
